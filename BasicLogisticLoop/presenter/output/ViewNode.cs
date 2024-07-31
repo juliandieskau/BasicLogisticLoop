c7d9a7770493785be92f03ede94ec69c94d069b1
WIN11-DEV2022\Admin <->
2024-07-31 09:32:37 +0200

    Add project files.

---
+++ BasicLogisticLoop/presenter/output/ViewNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogisticLoop.presenter.output
+{
+    internal struct ViewNode
+    {
+        private NodeType Type;
+
+    }
+}
