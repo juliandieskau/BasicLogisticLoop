368ed619bd6175b93a9c8b1c3d9b88c5bcd53f11
WIN11-DEV2022\Admin <->
2024-08-05 17:02:29 +0200

    Add OnLabelClick() method to be called from all Labels when clicked on them.

--- BasicLogisticLoop/BasicLogisticLoop.cs
+++ BasicLogisticLoop/BasicLogisticLoop.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,7 +15,7 @@ namespace BasicLogisticLoop
 {
     /// <summary>
     /// View of the BasicLogisticLoop. Initializes the Presenter which then calls the view to updates it with the model.
-    /// Handles user input and output in a GUI.
+    /// Handles user input and output in a GUI. InitializeView() needs to be called once.
     /// </summary>
     public partial class BasicLogisticLoopForm : Form
     {
@@ -39,14 +40,54 @@ namespace BasicLogisticLoop
         }
 
         /// <summary>
-        /// 
+        /// Takes a list of ViewNodes and constructs the view on the basis of it. Needs to be called once for the view to work.
         /// </summary>
-        /// <param name="initialViewNodes">List of </param>
+        /// <param name="initialViewNodes">List of ViewNodes to use as a basis to build the View out of.</param>
         public void InitializeView(List<ViewNode> initialViewNodes)
         {
             NodeData = initialViewNodes;
 
             // TODO generate Labels and InputWindows here
+            throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Takes a list of nodes that contains nodes that have already been initialized in the view and updates their state to the given one.
+        /// Updates the GUI Label accordingly.
+        /// </summary>
+        /// <param name="changedNodes">List of changedNodes to be updated.</param>
+        public void UpdateWithViewNodes(List<ViewNode> changedNodes)
+        {
+            foreach (ViewNode changedNode in changedNodes)
+            {
+                ViewNode nodeToUpdate = NodeData.Find(oldNode => oldNode.NodeID == changedNode.NodeID);
+                nodeToUpdate = changedNode;
+                UpdateNode(nodeToUpdate);
+            }
+        }
+
+        /// <summary>
+        /// Updates the label in the view representing the given ViewNode with its data.
+        /// </summary>
+        /// <param name="node">ViewNode to update in the view.</param>
+        private void UpdateNode(ViewNode node)
+        {
+            // TODO find Label matching the given nodes ID
+
+            // update the labels content with the data of the node
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Event handler to be added to all Labels that calls the Presenter by building a fitting IInput object.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnLabelClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
