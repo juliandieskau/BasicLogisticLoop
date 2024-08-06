bc0f2f19835d6342e73b044ac504ec798d1ccfeb
WIN11-DEV2022\Admin <->
2024-08-06 10:55:45 +0200

    Declare and partially implement methods. Implement GenerateX() methods and various helper methods.

--- BasicLogisticLoop/BasicLogisticLoop.cs
+++ BasicLogisticLoop/BasicLogisticLoop.cs
@@ -30,6 +30,10 @@ namespace BasicLogisticLoop
         /// </summary>
         private List<ViewNode> NodeData;
 
+        private const string LabelBaseName = "Label";
+        private readonly Color LabelBackColor = Color.White;
+        private readonly Color WindowBackColor = Color.White;
+
         /// <summary>
         /// Constructor for the Form. Initializes the Presenter and gives it this Form as an argument.
         /// </summary>
@@ -81,13 +85,157 @@ namespace BasicLogisticLoop
         /// <summary>
         /// Event handler to be added to all Labels that calls the Presenter by building a fitting IInput object.
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">Label that was clicked.</param>
         /// <param name="e"></param>
         private void OnLabelClick(object sender, EventArgs e)
         {
+            Label label = sender as Label;
+            // For Step Label
+            // For all Node Labels: 
+            //      When clicked display its content in label on the side (ShowContent)
+            // For Commissioning, Retrieval:
+            //      When clicked also call 
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Display the content of a given node label in the GUIs sidebar
+        /// </summary>
+        /// <param name="label">Label to show the content of.</param>
+        private void ShowContent(Label label)
+        {
 
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Method to generate a label based on a ViewNode object.
+        /// </summary>
+        /// <param name="node">ViewNode object to represent as a label.</param>
+        private void GenerateNodeLabel(ViewNode node)
+        {
+            // Convert node data to string representation
+            string nodeType = NodeTypeToString(node.Type);
+            string nodeID = node.NodeID.ToString();
+            string containerTUI = "empty";
+            if (node.Container != null)
+            {
+               containerTUI = node.Container.TransportUnitNumber.ToString();
+            }
+
+            string nodeName = GetNodeLabelName(node.Type, node.NodeID);
+            string nodeText = nodeType + nodeID + "\n" + "Content: " + containerTUI;
+
+            // Create the label
+            CreateLabel(name: nodeName, text: nodeText);
+        }
+
+        /// <summary>
+        /// Method to generate a label to be used as a button to forward the model one step.
+        /// </summary>
+        private void GenerateStepLabel()
+        {
+            // Create the label
+            CreateLabel(name: "StepLabel", text: "One step forward..");
+        }
+
+        /// <summary>
+        /// Adapter-Method to create label objects in a specified format. Adds the label to the Form's Controls.
+        /// Always use this method to create labels!
+        /// </summary>
+        /// <param name="name">Name of the label to access it by.</param>
+        /// <param name="text">Text of th label to be displayed.</param>
+        /// <returns>Created label.</returns>
+        private void CreateLabel(string name, string text)
+        {
+            Label label = new Label
+            {
+                Name = name,
+                Text = text,
+                AutoSize = true,
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = LabelBackColor,
+                Cursor = Cursors.Hand,
+                Font = new Font(new FontFamily("Arial"), 16, FontStyle.Regular, GraphicsUnit.Pixel)
+            };
+
+            label.Click += new EventHandler(OnLabelClick);
+
+            Controls.Add(label);
+        }
+
+        /// <summary>
+        /// Method to create arrows that point between node labels to show to flow of containers in the model. 
+        /// </summary>
+        /// <param name="direction">One of the following strings: left, up, right, down, doubleHorizontal, doubleVertical</param>
+        /// <param name="index">Number of arrows added. (Start with 0)</param>
+        private void CreateArrowLabel(string direction, int index)
+        {
+            string arrow = "";
+            switch (direction)
+            {
+                case "left":
+                    arrow = "\u21D0"; break;
+                case "up":
+                    arrow = "\u21D1"; break;
+                case "right":
+                    arrow = "\u21D2"; break;
+                case "down":
+                    arrow = "\u21D3"; break;
+                case "doubleHorizontal":
+                    arrow = "\u21D4"; break;
+                case "doubleVertical":
+                    arrow = "\u21D5"; break;
+                default:
+                    throw new ArgumentException("Not a valid direction string.");
+            }
+
+            Label label = new Label
+            {
+                Name = direction + index.ToString(),
+                Text = arrow,
+                AutoSize = true,
+                BorderStyle = BorderStyle.None,
+                Cursor = Cursors.Default,
+                Font = new Font(new FontFamily("Arial"), 20, FontStyle.Regular, GraphicsUnit.Pixel)
+            };
+
+            Controls.Add(label);
+        }
+
+        /// <summary>
+        /// Method to determine the text a certain node should be displayed as in the view.
+        /// </summary>
+        /// <param name="type">NodeType to display as text.</param>
+        /// <returns>String to display.</returns>
+        private string NodeTypeToString(NodeType type)
+        {
+            switch (type)
+            {
+                case NodeType.Conveyor:
+                    return "";
+                case NodeType.Retrieval:
+                    return "R";
+                case NodeType.Storage:
+                    return "S";
+                case NodeType.Commissioning:
+                    return "C";
+                case NodeType.Warehouse:
+                    return "W";
+                default:
+                    return "";
+            }
+        }
+
+        /// <summary>
+        /// Makes a string to use as names in labels for nodes of the model.
+        /// </summary>
+        /// <param name="type">NodeType to show which kind of node it is.</param>
+        /// <param name="nodeID">ID to represent which node in the model it belongs to.</param>
+        /// <returns></returns>
+        private string GetNodeLabelName(NodeType type, int nodeID)
+        {
+            return NodeTypeToString(type) + LabelBaseName + nodeID.ToString();
+        }
     }
 }
